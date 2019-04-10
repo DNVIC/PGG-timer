6d34eecdcabac59123a6d2ab2c1ed59759c19c2e
DNVIC <37513362+DNVIC@users.noreply.github.com>
2019-04-10 14:01:29 -0400

    Update time.py

--- time.py
+++ time.py
@@ -2,23 +2,21 @@ from time import sleep
 from datetime import datetime
 from pytz import timezone
 
-textFile2 = open("tz.txt", 'w')
 textFile = open("time.txt", 'w')
 count = 0
 constant = 0
 timeList = ['America/New_York', 'UTC', 'Europe/Berlin', 'US/Pacific']
 listNum = 0
 timeZone
-while constant = 0:
+while constant == 0:
   count = 0
   if listNum == 3:
     listNum = 0
   else:
-    listNum += 1
+    listNum = listNum + 1
   timeZone = timeList[listNum]
-  textFile2.write(timeZone)
   while count < 100:
     nowTime = datetime.now(timezone(timeZone))
     textFile.write(nowTime.strftime(fmt))
-    count +=1
+    count = count + 1
     sleep(0.1)
